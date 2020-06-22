bba48af04a7880df7b616259714d7ecf7cfd238a
ViniciusLeonardovky <vile.pb@gmail.com>
2020-06-22 17:55:12 -0300

    finished

--- src/modules/orders/infra/http/controller/OrdersController.ts
+++ src/modules/orders/infra/http/controller/OrdersController.ts
@@ -8,9 +8,24 @@ import FindOrderService from '@modules/orders/services/FindOrderService';
 export default class OrdersController {
   public async show(request: Request, response: Response): Promise<Response> {
     // TODO
+    const { id } = request.params;
+    const findOrderService = container.resolve(FindOrderService);
+    const order = await findOrderService.execute({ id });
+
+    return response.status(200).json({
+      customer: order?.customer,
+      order_products: order?.order_products,
+    });
   }
 
   public async create(request: Request, response: Response): Promise<Response> {
     // TODO
+    const { customer_id, products } = request.body;
+
+    const createOrder = container.resolve(CreateOrderService);
+
+    const order = await createOrder.execute({ customer_id, products });
+
+    return response.json(order);
   }
 }
--- src/modules/products/infra/typeorm/repositories/ProductsRepository.ts
+++ src/modules/products/infra/typeorm/repositories/ProductsRepository.ts
@@ -22,20 +22,35 @@ class ProductsRepository implements IProductsRepository {
     quantity,
   }: ICreateProductDTO): Promise<Product> {
     // TODO
+
+    const product = this.ormRepository.create({ name, price, quantity });
+
+    await this.ormRepository.save(product);
+
+    return product;
   }
 
   public async findByName(name: string): Promise<Product | undefined> {
     // TODO
+    const product = await this.ormRepository.findOne({ where: { name } });
+
+    return product;
   }
 
   public async findAllById(products: IFindProducts[]): Promise<Product[]> {
     // TODO
+    const productsList = await this.ormRepository.find({
+      where: In(products.map(product => product.id)),
+    });
+
+    return productsList;
   }
 
   public async updateQuantity(
     products: IUpdateProductsQuantityDTO[],
   ): Promise<Product[]> {
     // TODO
+    return this.ormRepository.save(products);
   }
 }
 
